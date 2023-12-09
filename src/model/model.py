213090554a7df87413b818c972ad05bbddb40a25
Ivan Baklanov <nymless.ib@gmail.com>
2023-12-09 18:17:00 +0500

    Разделил загрузчик модели на две функции. Добавил тесты.

--- src/model/model.py
+++ src/model/model.py
@@ -2,20 +2,37 @@ from transformers import pipeline
 
 
 def load_model():
-    """Загрузка предобученной модели для суммаризации текста. При использовании через Streamlit может потребоваться кеширование.
+    """Загрузка предобученной модели для суммаризации текста.
 
-    Returns:
-    Функция суммаризации - summarize (text: str) -> str.
+    Returns
+    -------
+    summarizer: Pipeline
+        Функция Pipeline суммаризации текста.
     """
 
     model_name = "IlyaGusev/mbart_ru_sum_gazeta"
     summarizer = pipeline(
         "summarization", model=model_name, tokenizer=model_name)
+    return summarizer
 
-    def summarize(text):
-        summary = summarizer(text, max_length=600, min_length=50,
-                             length_penalty=2.0, no_repeat_ngram_size=4)
-        text = summary[0]['summary_text']
-        return text
 
-    return summarize
+def get_text(summarizer, text):
+    """Функция суммаризации текста.
+
+    Parameters
+    ----------
+    summarizer : Pipeline
+        Функция возвращаемая load_model() -> Pipeline
+    text : str
+        Текст для обработки моделью.
+
+    Returns
+    -------
+    sum_text : str
+        Суммаризированный текст.
+    """
+
+    summary = summarizer(text, max_length=600, min_length=50,
+                         length_penalty=2.0, no_repeat_ngram_size=4)
+    sum_text = summary[0]['summary_text']
+    return sum_text
