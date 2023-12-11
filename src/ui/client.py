1731f575c600e19bfa1b5c5d7faa343fcabc4442
Alex48Yakunin <sanchezz.jakunin@rambler.ru>
2023-12-11 22:24:04 +0300

    adds ui + tests and connects model

---
+++ src/ui/client.py
@@ -0,0 +1,23 @@
+from transformers import pipeline
+import torch
+import streamlit as st
+import sys
+import os
+# model_path необходимо установить исходя из директории откуда запусается приложение
+# дефолтный запуск приложения из src "streamlit run src/ui/client.py"
+model_path = os.path.abspath('src/model')
+sys.path.append(model_path)
+from model import load_model, get_text
+
+st.title('Краткий пересказ текстов')
+# получаем текст от клиента
+text = st.text_area('Введите текст')
+
+# запускаем модель
+summary = load_model()
+
+st.write('Краткий пересказ')
+
+if st.button('Применить'):
+   # вывод решения на экран
+   st.success(get_text(summary, text))
