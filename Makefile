e8bca537975b40978327e233f02bf3c63bdaf24a
m.domchenko <m.domchenko@cyrm.ru>
2023-12-13 04:04:09 +0300

    [sprint_2-domchenko]: Makefile + requirements

---
+++ Makefile
@@ -0,0 +1,35 @@
+# Подготовка виртуального окружения
+.PHONY: venv
+venv:
+	python3 -m venv ./venv
+
+# Установка указанных пакетов (когда отсутствует файл `requirements.txt`)
+.PHONY: direct_deps
+direct_deps: venv
+	(. ./venv/bin/activate; pip3 install \
+		transformers sentencepiece streamlit torch pytest)
+
+# Установка пакетов указанных в `requirements.txt`
+.PHONY: deps
+deps: venv
+	(. ./venv/bin/activate; pip install -r requirements.txt)
+
+# Удаление виртуального окружения
+.PHONY: del_deps
+del_deps:
+	rm -R ./venv
+
+# Сохранение зависимостей в файл `requirements.txt`
+.PHONY: freeze
+freeze:
+	(. ./venv/bin/activate; pip freeze > requirements.txt)
+
+# Запуск
+.PHONY: run
+run:
+	(. ./venv/bin/activate; streamlit run src/ui/client.py)
+
+# Запуск тестов
+.PHONY: test
+test:
+	(. ./venv/bin/activate; pytest)
--- src/ui/client.py
+++ src/ui/client.py
@@ -1,13 +1,20 @@
-from transformers import pipeline
-import torch
 import streamlit as st
+import torch
 import sys
 import os
+
 # model_path необходимо установить исходя из директории откуда запусается приложение
 # дефолтный запуск приложения из src "streamlit run src/ui/client.py"
 model_path = os.path.abspath('src/model')
 sys.path.append(model_path)
+
 from model import load_model, get_text
+
+# определяем на чем будем запускать модель
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
 
 st.title('Краткий пересказ текстов')
 # получаем текст от клиента
@@ -20,5 +27,5 @@ summary = load_model()
 st.write('Краткий пересказ')
 
 if st.button('Применить'):
-   # вывод решения на экран
-   st.success(get_text(summary, text))
+    # вывод решения на экран
+    st.success(get_text(summary, text))
