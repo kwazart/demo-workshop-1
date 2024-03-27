476a64cd6da3acb00a9e37b009da9973c4ac31cd
artempolozov <i@apolozov.ru>
2024-03-27 13:41:11 +0300

    test refactoring - 2

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,9 @@
 from src.model.model import load_tokenizer, load_bart_model,  make_summary_text
+import os
+
 
 tokenizer = load_tokenizer()
 model = load_bart_model()
-
-text_1 = open("./resources/text_1.txt", "r").read()
-text_2 = open("./resources/text_2.txt", "r").read()
 
 
 def test_load_tokenizer():
@@ -16,6 +15,11 @@ def test_load_bart_model():
 
 
 def test_get_text():
+    with open(os.path.join("tests", "resources", "text_1.txt"), mode='r', encoding="utf_8_sig") as f:
+        text_1 = f.read()
+    with open(os.path.join("tests", "resources", "text_2.txt"), mode='r', encoding="utf_8_sig") as f:
+        text_2 = f.read()
+
     summary = make_summary_text(tokenizer, model, text_1)
     assert isinstance(summary, str)
     # Данный текст длиннее чем может обработать модель.
